3ddd620a77ecb0d48b4a05533350b9b60e8ce980
Vlad Kelar <38537230+LeadEx13@users.noreply.github.com>
2024-07-18 19:16:32 +0300

    lint fix

--- src/tests/App.spec.js
+++ src/tests/App.spec.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import App from '../App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import App from '../App'
 
 describe('Render login inputs', () => {
-  test('Username input should be in the document', () => {
-    render(<App />);
-    expect(screen.getByTestId('identifierLoginInput')).toBeInTheDocument();
-  });
+    test('Username input should be in the document', () => {
+        render(<App />)
+        expect(screen.getByTestId('identifierLoginInput')).toBeInTheDocument()
+    })
 
-  test('Password input should be in the document', () => {
-    render(<App />);
-    expect(screen.getByTestId('passwordLoginInput')).toBeInTheDocument();
-  });
+    test('Password input should be in the document', () => {
+        render(<App />)
+        expect(screen.getByTestId('passwordLoginInput')).toBeInTheDocument()
+    })
 
-  test('Submit input button should be in the document', () => {
-    render(<App />);
-    expect(screen.getByRole('button', { name: /log in/i })).toBeInTheDocument();
-  });
-});
+    test('Submit input button should be in the document', () => {
+        render(<App />)
+        expect(screen.getByRole('button', { name: /log in/i })).toBeInTheDocument()
+    })
+})

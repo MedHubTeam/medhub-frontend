c264d99c510f178d619abd77b0a784a38d4b5fe5
NadavMozeson <nadavson02@gmail.com>
2024-07-18 14:57:27 +0300

    Update loginService.js

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,9 +1,7 @@
-const loginRequest = async (username, password) => {
+module.exports = async (username, password) => {
     const url = `https://medhub-backend.onrender.com/login?username=${username}&password=${password}`
     const response = await fetch(url, {
         method: 'GET'
     })
     return await response.json()
 }
-
-export default loginRequest

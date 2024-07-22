94d7279316d2db874a8a0ca441e5327141807487
NadavMozeson <nadavson02@gmail.com>
2024-07-22 11:07:33 +0300

    Removed register route temporarily

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 // Import jsx components
 import LoginPage from './context/loginPage'
-import RegisterPage from './context/registerPage'
 import HomePage from './context/homePage'
 import AboutPage from './context/aboutPage'
 
@@ -12,10 +11,9 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/" element={<LoginPage />} />
                 <Route path="/home" element={<HomePage />} />
                 <Route path="/about" element={<AboutPage />} />
-                <Route path="/" element={<LoginPage />} />
             </Routes>
         </Router>
     )

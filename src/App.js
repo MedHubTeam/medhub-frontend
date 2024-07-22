68cb22a4c3ca37e12dc431fd9d70092266836e8b
NadavMozeson <nadavson02@gmail.com>
2024-07-22 11:18:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 // Import jsx components
 import LoginPage from './context/loginPage'
+import RegisterPage from './context/registerPage'
 import HomePage from './context/homePage'
 import AboutPage from './context/aboutPage'
 
@@ -11,7 +12,8 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<LoginPage />} />
+                <Route path="/" exact element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
                 <Route path="/home" element={<HomePage />} />
                 <Route path="/about" element={<AboutPage />} />
             </Routes>

ec9e21278bcc28dfc9e33dd02ebf78dc5fbbae73
NadavMozeson <nadavson02@gmail.com>
2024-07-31 16:39:31 +0300

    Added register page tests

---
+++ src/tests/registerPage.test.js
@@ -0,0 +1,37 @@
+// Import react library and react testing library
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+
+// Import helper functions to use for testing
+import { elementExists, RouterRender } from './testHelperFunctions'
+
+// Import Pages and components to run tests on
+import RegisterPage from '../context/registerPage'
+
+
+describe('Render register inputs', () => {
+    test('Username input should be in the document', () => {
+        RouterRender(<RegisterPage />)
+        expect(elementExists('identifierRegisterInput')).toBe(true)
+    })
+    test('Email input should be in the document', () => {
+        RouterRender(<RegisterPage />)
+        expect(elementExists('emailRegisterInput')).toBe(true)
+    })
+    test('Password input should be in the document', () => {
+        RouterRender(<RegisterPage />)
+        expect(elementExists('passwordRegisterInput')).toBe(true)
+    })
+    test('Confirm Password input should be in the document', () => {
+        RouterRender(<RegisterPage />)
+        expect(elementExists('confirmPasswordRegisterInput')).toBe(true)
+    })
+    test('Profession input should be in the document', () => {
+        RouterRender(<RegisterPage />)
+        expect(elementExists('userProfessionRegisterInput')).toBe(true)
+    })
+    test('Submit input button should be in the document', () => {
+        RouterRender(<RegisterPage />)
+        expect(elementExists('submitRegisterInputButton')).toBe(true)
+    })
+})

133b4f785ce371430125abe8e8775bed88af5dc4
nativlevi <nativl12345@gmail.com>
2024-08-15 23:47:09 +0300

    Fixed tests

--- src/tests/homePage.test.js
+++ src/tests/homePage.test.js
@@ -15,29 +15,4 @@ describe('HomePage', () => {
         expect(elementExists('identifierPostInput')).toBe(true)
         expect(elementExists('submitPostInputButton')).toBe(true)
     })
-
-    test('renders edit post inputs', async () => {
-        await RenderHomePage()
-        expect(elementAllExist('submitEditPostInputButton')).toBe(true)
-    })
-
-    test('renders edit post content input after clicking edit', async () => {
-        await RenderHomePage()
-        const editButtons = screen.getAllByTestId('submitEditPostInputButton')
-        await userEvent.click(editButtons[0])
-        expect(elementExists('identifierEditPostInput')).toBe(true)
-        expect(elementExists('submitSaveEditPostInputButton')).toBe(true)
-    })
-
-    test('renders cancel edit post inputs after clicking edit', async () => {
-        await RenderHomePage()
-        const editButtons = screen.getAllByTestId('submitEditPostInputButton')
-        await userEvent.click(editButtons[0])
-        expect(elementExists('submitCancelEditInputButton')).toBe(true)
-    })
-
-    test('renders delete post inputs', async () => {
-        await RenderHomePage()
-        expect(elementAllExist('identifierDeletePostInput')).toBe(true)
-    })
 })
